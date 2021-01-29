bee7694b13f19f92b2056f48dd71ea6f546973a0
MegSemrad <mlsemrad753@gmail.com>
2021-01-29 15:38:02 -0600

    login and register

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+// json-server -p 8090 -w database.json
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { PatientApp } from "./components/PatientApp";   {/*Remember to change later to my app name!!! */}
-import './index.css';
 
 
 ReactDOM.render(
