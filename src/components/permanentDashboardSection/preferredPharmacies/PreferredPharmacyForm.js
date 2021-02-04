e48e967469cb7521025c1a151b91b26586a28798
MegSemrad <mlsemrad753@gmail.com>
2021-02-04 10:41:21 -0600

    left side forms all completely work

--- src/components/permanentDashboardSection/preferredPharmacies/PreferredPharmacyForm.js
+++ src/components/permanentDashboardSection/preferredPharmacies/PreferredPharmacyForm.js
@@ -36,44 +36,44 @@ export const PreferredPharmacyForm = () => {
                 emergencyContactRelation: basePatientDetails.emergencyContactRelation,
                 emergencyContactPhoneNumber: basePatientDetails.emergencyContactPhoneNumber,
                 preferredPharmacyName: basePatientDetails.preferredPharmacyName,
-                preferredPharmacyAddress: basePatientDetails.referredPharmacyAddress,
+                preferredPharmacyAddress: basePatientDetails.preferredPharmacyAddress,
                 preferredPharmacyPhoneNumber: basePatientDetails.preferredPharmacyPhoneNumber
             })
                 .then(() => history.push("/home"))
-        } else {
-            addBasePatientDetails({
-                userId: userId, //what to save the userId who is currently logged in on the new basePatientDetails object
-                name: basePatientDetails.name,
-                birthday: basePatientDetails.birthday,
-                conditions: basePatientDetails.conditions,
-                allergies: basePatientDetails.allergies,
-                emergencyContactName: basePatientDetails.emergencyContactName,
-                emergencyContactRelation: basePatientDetails.emergencyContactRelation,
-                emergencyContactPhoneNumber: basePatientDetails.emergencyContactPhoneNumber,
-                preferredPharmacyName: basePatientDetails.preferredPharmacyName,
-                preferredPharmacyAddress: basePatientDetails.referredPharmacyAddress,
-                preferredPharmacyPhoneNumber: basePatientDetails.preferredPharmacyPhoneNumber
-            })
-                .then(() => history.push("/home"))
-        }
-    }
-
-
-
-
-
-
-    useEffect(() => {
-        getBasePatientDetails().then(() => {
-            if (basePatientDetailsId) {
-                getBasePatientDetailsById(basePatientDetailsId).then(() => {
-                    setIsLoading(false)
-                })
             } else {
-                setIsLoading(false)
+                addBasePatientDetails({
+                    userId: userId, //what to save the userId who is currently logged in on the new basePatientDetails object
+                    name: basePatientDetails.name,
+                    birthday: basePatientDetails.birthday,
+                    conditions: basePatientDetails.conditions,
+                    allergies: basePatientDetails.allergies,
+                    emergencyContactName: basePatientDetails.emergencyContactName,
+                    emergencyContactRelation: basePatientDetails.emergencyContactRelation,
+                    emergencyContactPhoneNumber: basePatientDetails.emergencyContactPhoneNumber,
+                    preferredPharmacyName: basePatientDetails.preferredPharmacyName,
+                    preferredPharmacyAddress: basePatientDetails.preferredPharmacyAddress,
+                    preferredPharmacyPhoneNumber: basePatientDetails.preferredPharmacyPhoneNumber
+                })
+                    .then(() => history.push("/home"))
             }
-        })
-    }, [])
+        }
+
+
+
+
+
+
+        useEffect(() => {
+            getBasePatientDetails().then(() => {
+                if (basePatientDetailsId) {
+                    getBasePatientDetailsById(basePatientDetailsId).then(() => {
+                        setIsLoading(false)
+                    })
+                } else {
+                    setIsLoading(false)
+                }
+            })
+        }, [])
 
 
 
@@ -87,8 +87,8 @@ export const PreferredPharmacyForm = () => {
             <h2 className="preferredPharmaciesForm__title">Preferred Pharmacy</h2>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name"
+                    <label htmlFor="preferredPharmacyName">Name:</label>
+                    <input type="text" id="preferredPharmacyName"
                         onChange={handleControlledInputChange}
                         required autoFocus
                         className="form-control"
@@ -98,8 +98,8 @@ export const PreferredPharmacyForm = () => {
             </fieldset>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="address">Address: </label>
-                    <input type="text" id="address"
+                    <label htmlFor="preferredPharmacyAddress">Address: </label>
+                    <input type="text" id="preferredPharmacyAddress"
                         onChange={handleControlledInputChange}
                         required autoFocus
                         className="form-control"
@@ -109,8 +109,8 @@ export const PreferredPharmacyForm = () => {
             </fieldset>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="phoneNumber">Phone Number: </label>
-                    <input type="text" id="phoneNumber"
+                    <label htmlFor="preferredPharmacyPhoneNumber">Phone Number: </label>
+                    <input type="text" id="preferredPharmacyPhoneNumber"
                         onChange={handleControlledInputChange}
                         required autoFocus
                         className="form-control"
