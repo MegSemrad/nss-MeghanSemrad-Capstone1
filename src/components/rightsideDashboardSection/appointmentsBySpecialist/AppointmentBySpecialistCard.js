37c929aef2be88bf536eca526b1786f1ed751cc9
MegSemrad <mlsemrad753@gmail.com>
2021-02-04 14:14:34 -0600

    working on appt

---
+++ src/components/rightsideDashboardSection/appointmentsBySpecialist/AppointmentBySpecialistCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "react-bootstrap";
+
+
+export const AppointmentBySpecialistCard = ({ ?? }) => {
+  return (
+    <Button variant="primary" size="lg" block 
+    onClick={() => { history.push(`/AppointmentBySpecialist/edit/${??.id}`)>
+        {??.speciality}
+    </Button>
+)}
+
--- src/components/rightsideDashboardSection/appointmentsBySpecialist/AppointmentBySpecialistProvider.js
+++ src/components/rightsideDashboardSection/appointmentsBySpecialist/AppointmentBySpecialistProvider.js
@@ -1,1 +1,39 @@
 import React, { useState, createContext } from "react";
+
+export const AppointmentBySpecialistContext = createContext();
+
+
+export const AppointmentBySpecialistProvider = (props) => {
+    const [appointmentsBySpecialist, setAppointmentsBySpecialist] = useState([])
+
+    const getappointmentsBySpecialist = () => {
+        return fetch ("http://localhost:8090/appointmentsBySpecialist?_expand=specialistType")
+        .then(res => res.json())
+        .then(setAppointmentsBySpecialist)
+    }
+
+    const addAppointmentBySpecialist = appointmentBySpeicalistObj => {
+        return fetch("http://localhost:8090/appointmentsBySpecialist", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(appointmentBySpeicalistObj)
+        })
+        .then(getappointmentsBySpecialist)
+    };
+
+
+    const getAppointmentBySpecialistById= (id) => {
+        return fetch(`http://localhost:8090/appointmentsBySpecialist/${id}?_expand=specialistType`)
+            .then(res => res.json())
+    };
+
+    return (
+        <AppointmentBySpecialistContext.Provider value={{
+            appointmentsBySpecialist, getappointmentsBySpecialist, setAppointmentsBySpecialist, addAppointmentBySpecialist, getAppointmentBySpecialistById
+        }}>
+         {props.children}
+        </AppointmentBySpecialistContext.Provider>
+    )
+};
