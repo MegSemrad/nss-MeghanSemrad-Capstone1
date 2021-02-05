390b8b37ce9bca7539a2be7f092dd81268ba04b1
MegSemrad <mlsemrad753@gmail.com>
2021-02-05 16:06:41 -0600

    saving for weekend

--- src/components/rightsideDashboardSection/specialistTypes/SpecialistTypeCard.js
+++ src/components/rightsideDashboardSection/specialistTypes/SpecialistTypeCard.js
@@ -3,11 +3,12 @@ import { useHistory } from "react-router-dom"
 import { Button } from "react-bootstrap";
 
 export const SpecialistTypeCard = ({ specialistType }) => {
-    const history = useHistory()
+  const history = useHistory()
   return (
-    <Button variant="primary" size="lg" block className="rightSideChildCSS"
-    onClick={() => { history.push(`/SpecialistType/detail/${specialistType.id}`)}}>
+      <Button variant="primary" size="lg" block className="rightSideChildCSS"
+        onClick={() => { history.push(`/SpecialistType/detail/${specialistType.id}`) }}>
         {specialistType.speciality}
-    </Button>
-)}
+      </Button>
+  )
+}
 
--- src/components/rightsideDashboardSection/specialistTypes/SpecialistTypeList.js
+++ src/components/rightsideDashboardSection/specialistTypes/SpecialistTypeList.js
@@ -1,28 +1,28 @@
 import React, { useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom" 
+import { useHistory } from "react-router-dom"
 import { SpecialistTypeContext } from "./SpecialistTypeProvider";
 import { SpecialistTypeCard } from "./SpecialistTypeCard";
 
-export const SpecialistTypeList= () => {
+export const SpecialistTypeList = () => {
   const { specialistTypes, getSpecialistTypes } = useContext(SpecialistTypeContext)
   const history = useHistory()
 
   useEffect(() => {
     getSpecialistTypes()
-    }, []);
+  }, []);
 
 
   return (
     <div className="specialistTypes">
         <h2 className="titleWord">Appointments</h2>
-		    <button onClick={() => {history.push("/SpecialistType/create")}}>
-                Add
-            </button>
-        {specialistTypes.map(specialistType => {
+          <button onClick={() => { history.push("/SpecialistType/create") }}>
+            Add
+          </button>
+          {specialistTypes.map(specialistType => {
             return <SpecialistTypeCard key={specialistType.id} //argument
-            specialistType={specialistType} /> //argument
-        })
-        }
-    </div>
+              specialistType={specialistType} /> //argument
+          })
+          }
+        </div>
   )
 };
