6319179634d3b574bd113c939c6174229bc31464
MegSemrad <mlsemrad753@gmail.com>
2021-02-05 11:56:50 -0600

    css

--- src/components/rightsideDashboardSection/appointmentsBySpecialist/AppointmentBySpecialistCard.js
+++ src/components/rightsideDashboardSection/appointmentsBySpecialist/AppointmentBySpecialistCard.js
@@ -5,7 +5,7 @@ import { Button } from "react-bootstrap";
 export const AppointmentBySpecialistCard = ({ appointmentBySpecialist }) => {
     const history = useHistory()
   return (
-    <Button variant="primary" size="lg" block 
+    <Button variant="primary" size="lg" block className="rightSideChildCSS"
     onClick={() => { history.push(`/AppointmentBySpecialist/detail/${appointmentBySpecialist.id}`)}}>
         {appointmentBySpecialist.specialistType?.speciality}
     </Button>
