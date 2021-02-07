6e63851f746e6433f93340e978b76d94a387c475
MegSemrad <mlsemrad753@gmail.com>
2021-02-07 14:20:17 -0600

    added doctor name to card and details

--- src/components/rightsideDashboardSection/specialistTypes/SpecialistTypeDetail.js
+++ src/components/rightsideDashboardSection/specialistTypes/SpecialistTypeDetail.js
@@ -34,7 +34,7 @@ export const SpecialistTypeDetail = () => {
   
 return (
     <section className="specialistType">
-      <h3 className="specialistType__name">{specialist.speciality}</h3>
+      <h3 className="specialistType__name">{specialist.speciality} ({specialist.specialistName})</h3>
       <div className="specialistType__appointmentNote__and__date">Appointment Note: {specialist.appointmentsBySpecialist?.map(note => {return note.appointmentNote})} {specialist.appointmentsBySpecialist?.map(date => {return date.appointmentDate})}</div>
       <div className="specialistType__questions">Questions: {specialist.questions?.map(question => {return question.questions})}</div>
       {/* <button onClick={handleRelease}>Delete</button> */}
