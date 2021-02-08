589af5ca1a12efe66b1771bb827df59933ae1831
MegSemrad <mlsemrad753@gmail.com>
2021-02-08 09:16:59 -0600

    family history provider

---
+++ src/components/rightsideDashboardSection/familyHistories/FamilyHistoryProvider.js
@@ -0,0 +1,67 @@
+import React, { useState, createContext } from "react";
+
+export const familyHistoriesContext = createContext();
+
+export const FamilyHistoryProvider = (props) => {
+    const [familyHistory, setFamilyHistory] = useState([])
+    const [familyHistories, setFamilyHistories] = useState([]) //if ever need all users' family histories
+
+    const getFamilyHistories = () => {
+        return fetch("http://localhost:8090/familyHistories")
+        .then(response => response.json())
+        .then(setFamilyHistories)
+    };
+
+
+    const getFamilyHistoryById = (id) => {
+        return fetch(`http://localhost:8090/familyHistories${id}`)
+        .then(response => response.json())
+        .then((familyHistory) => {
+            console.log("family history?", familyHistory)
+            setFamilyHistory
+        })
+    };
+
+
+    const addFamilyHistory = (familyHistoryObject) => {
+        return fetch("http://localhost:8090/familyHistories", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(familyHistoryObject)
+        })
+        .then(getFamilyHistories) //should it be getFamilyHistories??? or singular
+    };
+
+
+    const updateFamilyHistory = (familyHistory) => {
+        return fetch(`http://localhost:8088/animals/${familyHistoy.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(familyHistory)
+    })
+        .then(getFamilyHistories)
+    };
+
+
+    const deleteFamilyHistory = (familyHistoryId) => {
+        return fetch(`http://localhost:8090/familyHistories/${familyHistoryId}`, {
+        method: "DELETE"
+        })
+        .then(getFamilyHistories)
+    };
+
+
+    return (
+        <familyHistoriesContext.Provider value={{
+            familyHistory, setFamilyHistory, familyHistories, setFamilyHistories,
+            getFamilyHistories, getFamilyHistoryById, addFamilyHistory, updateFamilyHistory,
+            deleteFamilyHistory
+        }}>
+            {props.children}
+        </familyHistoriesContext.Provider>
+    )
+};
