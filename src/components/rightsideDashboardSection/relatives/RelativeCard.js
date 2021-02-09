25f3890b1b25502b79de9ec0947af50c6670a86b
MegSemrad <mlsemrad753@gmail.com>
2021-02-08 21:02:55 -0600

    added bullet points and className for formatting

--- src/components/rightsideDashboardSection/relatives/RelativeCard.js
+++ src/components/rightsideDashboardSection/relatives/RelativeCard.js
@@ -7,12 +7,18 @@ export const RelativeCard = ({ matchedRelative }) => {
   const history = useHistory()
 
   return (
-    <section className="relative">
+    <section className="relative rightSideChildCSS">
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>{matchedRelative.relative?.relative}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">Conditions</Card.Subtitle>
-          <Card.Text>{matchedRelative.condition}</Card.Text>
+          <Card.Text>
+            <ul>
+              <li>
+                {matchedRelative.condition}
+              </li>
+            </ul>
+          </Card.Text>
           <Button onClick={() => { history.push(`/relative/edit/${matchedRelative.id}`) }}>
             Edit
           </Button>
